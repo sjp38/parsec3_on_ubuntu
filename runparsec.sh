f69e7dd2a1d59ef4f630f4441e946adf980d3c43
SeongJae Park <sj38.park@gmail.com>
2017-09-24 10:36:30 +0900

    Add parsec runner script
    Signed-off-by: sjpark <sjpark@hydra.hydra>

---
+++ runparsec.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [ $# -ne 1 ];
+then
+	echo "Usage: $0 <workload>"
+	echo ""
+	echo "workload: PARSEC package.  e.g., canneal" 
+	exit 1
+fi
+
+CALLEE_DIR=`dirname $0`
+pushd $CALLEE_DIR > /dev/null
+
+PARSEC_DIR=parsec-3.0
+
+pushd $PARSEC_DIR > /dev/null
+source ./env.sh
+parsecmgmt -p $1 -a run -i native -n 1
+popd > /dev/null
+
+popd > /dev/null
