11a6d2542b0ee5ac805f01a3af8aec7b40dfaa09
SeongJae Park <sj38.park@gmail.com>
2020-06-23 06:10:43 +0200

    install_deps: Add 'pkg-config' package
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- install_deps.sh
+++ install_deps.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 sudo apt-get install -y libx11-dev libxext-dev libxmu-dev libxi-dev \
-	libtbb2 tbb-examples gcc-5 g++-5 gettext autoconf texinfo
+	libtbb2 tbb-examples gcc-5 g++-5 gettext autoconf texinfo pkg-config
