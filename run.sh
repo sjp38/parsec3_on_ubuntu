e030c69316964e21620c1801862a4438dd2b62f2
SeongJae Park <sj38.park@gmail.com>
2021-01-27 21:34:30 +0100

    run.sh: Fix shellcheck warnings
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- run.sh
+++ run.sh
@@ -8,14 +8,14 @@ then
 	exit 1
 fi
 
-CALLEE_DIR=`dirname $0`
-pushd $CALLEE_DIR > /dev/null
+CALLEE_DIR=$(dirname "$0")
+pushd "$CALLEE_DIR" > /dev/null
 
 PARSEC_DIR=parsec-3.0
 
-pushd $PARSEC_DIR > /dev/null
+pushd "$PARSEC_DIR" > /dev/null
 source ./env.sh
-parsecmgmt -p $1 -a run -i native -n 1
+parsecmgmt -p "$1" -a run -i native -n 1
 popd > /dev/null
 
 popd > /dev/null
