47812ee057f2caa65cec2e256a5c84aa73895121
SeongJae Park <sj38.park@gmail.com>
2022-04-21 12:26:25 +0000

    Support Ubuntu 20.04
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- _patch.sh
+++ _patch.sh
@@ -21,6 +21,12 @@ then
 	patch -p1 < ../for-ubuntu18.04.patch
 fi
 
+if [ "$(cat /etc/issue)" = "Ubuntu 20.04.4 LTS \n \l" ];
+then
+	echo "This system seems Ubuntu 20.04"
+	patch -p1 < ../for-ubuntu20.04.patch
+fi
+
 popd
 
 popd
