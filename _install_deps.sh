96dd52b32ab5baa5f5eaac9740a23a6a085821d4
SeongJae Park <sj38.park@gmail.com>
2022-04-21 10:43:26 +0000

    _install_deps: Add libtbb-dev
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- _install_deps.sh
+++ _install_deps.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for pkg in libx11-dev libxext-dev libxmu-dev libxi-dev \
+for pkg in libx11-dev libxext-dev libxmu-dev libxi-dev libtbb-dev \
 	libtbb2 tbb-examples gcc-5 g++-5 gettext autoconf texinfo pkg-config
 do
 	sudo apt-get install -y "$pkg"
