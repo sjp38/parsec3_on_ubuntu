812b67790af8070a459c70485d0603170c069cf8
SeongJae Park <sj38.park@gmail.com>
2024-01-31 17:57:33 -0800

    _build: Skip failing builds on Debian 12
    On Debian 12, some workloads cannot be built.  Just skip building those
    for now.
    
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- _build.sh
+++ _build.sh
@@ -14,7 +14,24 @@ if [ $# -gt 0 ]
 then
 	parsecmgmt -a build -p "$1"
 else
-	parsecmgmt -a build
+	if grep --quiet "Debian GNU/Linux 12 " /etc/issue
+	then
+		# On the distro, building bodytrack, dedup, facesim, and
+		# splash2x.raytrace fails.  Just don't use those for now.
+		for workload in blackscholes canneal facesim fluidanimate \
+			freqmine raytrace streamcluster vips x264
+		do
+			parsecmgmt -a build -p "$workload"
+		done
+
+		for workload in barnes fft lu_cb lu_ncb ocean_cp ocean_ncp \
+			radiosity radix volrend water_nsquared water_spatial
+		do
+			parsecmgmt -a build -p "splash2x.$workload"
+		done
+	else
+		parsecmgmt -a build
+	fi
 fi
 
 popd
